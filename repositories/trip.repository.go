c442f45c08f87497426f2daf6daadcf28cbf5bc1
Nicolas Silva <nicolas.senac15@gmail.com>
2019-10-14 20:17:43 -0300

    separação dos modules

---
+++ repositories/trip.repository.go
@@ -0,0 +1,37 @@
+package repositories
+
+import (
+	"context"
+	"easymile-tracker/common/database"
+	"easymile-tracker/common/models"
+	"fmt"
+	"log"
+)
+
+// Save the trip
+func Save(trip *models.Trip) error {
+	client := database.Connection()
+	collection := client.Database("easymile").Collection("trip")
+	insertResult, err := collection.InsertOne(context.TODO(), trip)
+
+	if err != nil {
+		fmt.Print(err)
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+
+	err = client.Disconnect(context.TODO())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connection to MongoDB closed.")
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
